3cb4f273602a20d1802428cc4b4f11e256ef7594
aliso4ka <gulyayevaalisa@gmail.com>
2026-06-02 17:50:01 +0200

    added bgr

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/Services/Services";
@@ -5,14 +6,43 @@ import WhyUs from "./components/WhyUs/WhyUs";
 import "./App.css";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFadeOut(true);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 600);
+    }, 1400);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className={`loader ${fadeOut ? "fadeOut" : ""}`}>
+        <div className="loaderInner">
+          <div className="logoPulse">
+            <img src="/favicon.svg" alt="logo" />
+          </div>
+          <div className="loaderText">Východ Digital</div>
+          <div className="loaderLine"></div>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <>
+    <div className="appFadeIn">
       <Header />
       <main>
         <Hero />
         <Services />
         <WhyUs />
       </main>
-    </>
+    </div>
   );
 }
