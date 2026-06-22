76b058a17a31d6282c1468cba24a0507f52cd739
aliso4ka <gulyayevaalisa@gmail.com>
2026-06-22 19:03:49 +0200

    new project addeded

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,7 @@
 import project1_600 from "../../src/assets/project1-600.webp";
 import project1_800 from "../../src/assets/project1-800.webp";
+import project2_600 from "../../src/assets/project2-600.webp";
+import project2_800 from "../../src/assets/project2-800.webp";
 
 export const projects = [
   {
@@ -10,5 +12,14 @@ export const projects = [
     image: project1_600,
     image2x: project1_800,
     link: "https://happy-shark.pp.ua/",
+  },
+  {
+    id: 2,
+    title: "LumenFrame",
+    description:
+      "Moderná landing page s originálnym dizajnom a silnou vizuálnou identitou.",
+    image: project2_600,
+    image2x: project2_800,
+    link: "https://lumenframe-sable.vercel.app/",
   },
 ];
